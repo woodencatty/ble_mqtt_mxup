477d86a872caf4a279026b566992713abbb38d3d
woodencatty <woodencatty@gmail.com>
2019-01-03 16:08:25 +0900

    restore

--- noble/noble_readst.js
+++ noble/noble_readst.js
@@ -57,21 +57,16 @@ function onServicesAndCharacteristicsDiscovered(error, services, characteristics
   
   // create an interval to send data to the service
   let count = 0;
-setInterval(() => {
-  const message = new Buffer('hello, ble ' + count, 'utf-8');
-  console.log("Sending:  '" + message + "'");
-  echoCharacteristic.write(message, (error)=>{
-    console.log(error);
-  });
-}, 1000);
-
   setInterval(() => {
     count++;
- 
+    const message = new Buffer('hello, ble ' + count, 'utf-8');
+    console.log("Sending:  '" + message + "'");
     echoCharacteristic.read((error, data)=>{
       console.log(error);
       console.log(data);
     });
-
+    echoCharacteristic.write(message, (error)=>{
+      console.log(error);
+    });
   }, 2500);
 }
