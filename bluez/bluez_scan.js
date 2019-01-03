4cde259561018f23f2cf0eb80fbb4898e866f2fa
woodencatty <woodencatty@gmail.com>
2019-01-03 13:19:37 +0900

    ww

--- bluez/bluez_scan.js
+++ bluez/bluez_scan.js
@@ -18,7 +18,7 @@ bluetooth.init().then(async ()=>{
     // listen on first bluetooth adapter
     var adapter = await bluetooth.getAdapter('hci0');
     //await adapter.StartDiscovery();
-    await adapter.getDevice("*");
+    await adapter.getDevice("50:B7:3D:C9:2F:6F");
     console.log("Discovering");
 });
 
