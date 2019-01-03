662e4f0233d852efaa29eeff5ac80b6a7ffc0a1e
woodencatty <woodencatty@gmail.com>
2019-01-03 13:06:28 +0900

    ww

--- bluez/bluez_scan.js
+++ bluez/bluez_scan.js
@@ -4,18 +4,25 @@ const bluetooth = new Bluez();
  
 // Register callback for new devices
 bluetooth.on('device', async (address, props) => {
+    
     console.log("Found new Device " + address + " " + props.Name);
     if(props.Name == "Beoplay E8"){
+        var adapter_tmp = await bluetooth.getAdapter('hci0');
         console.log(props);
+        var device = bluetooth.gatDevice(address);
+        adapter_tmp.RemoveDevice(device);
     }
+    
 });
  
 
 // Initialize bluetooth interface
 bluetooth.init().then(async ()=>{
     // listen on first bluetooth adapter
-    const adapter = await bluetooth.getAdapter('hci0');
-    await adapter.RemoveDevice();
+    var adapter = await bluetooth.getAdapter('hci0');
+
     await adapter.StartDiscovery();
     console.log("Discovering");
 });
+
+await
