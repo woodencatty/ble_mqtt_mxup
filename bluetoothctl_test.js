c3f7ad3b707fb8762b63405e237a4acbbd2fac6b
woodencatty <woodencatty@gmail.com>
2019-01-03 11:50:52 +0900

    tst

--- bluetoothctl_test.js
+++ bluetoothctl_test.js
@@ -1,1 +1,35 @@
-wow!
+var blue = require("bluetoothctl");
+blue.Bluetooth()
+
+
+blue.on(blue.bluetoothEvents.Controller, function(controllers){
+console.log('Controllers:' + JSON.stringify(controllers,null,2))
+});
+
+
+blue.on(blue.bluetoothEvents.DeviceSignalLevel, function(devices,mac,signal){
+    console.log('signal level of:' + mac + ' - ' + signal)
+
+});
+
+blue.on(blue.bluetoothEvents.Device, function (devices) {
+    console.log('devices:' + JSON.stringify(devices,null,2))
+})
+
+blue.on(blue.bluetoothEvents.PassKey, function (passkey) {
+    console.log('Confirm passkey:' + passkey)
+    blue.confirmPassKey(true);
+})
+
+var hasBluetooth=blue.checkBluetoothController();
+console.log('system has bluetooth controller:' + hasBluetooth)
+
+if(hasBluetooth) {
+    console.log('isBluetooth Ready:' + blue.isBluetoothReady)
+    blue.scan(true)
+    setTimeout(function(){
+        console.log('stopping scan')
+        blue.scan(false)
+        blue.info('00:0C:8A:8C:D3:71')
+    },20000)
+}
